f8337d3cba511fe08bdfd5d429595e35e1943dd5
UIT-22730023 <22730023@ms.uit.edu.vn>
2024-05-08 07:57:25 +0700

    Update snake.py

--- snake.py
+++ snake.py
@@ -21,3 +21,26 @@ class Snake():
         {'x': self.x - 1, 'y': self.y},
         {'x': self.x - 2, 'y': self.y}
     ]
+
+  #cập nhật vị trí của con rắn và kiểm tra xem con rắn có ăn quả táo không
+  def update(self, apple):
+    # Nếu đầu con rắn trùng vị trí với quả táo (apple), quả táo được đặt lại ở vị trí mới. 
+    if self.wormCoords[self.HEAD]['x'] == apple.x and self.wormCoords[self.HEAD]['y'] == apple.y:
+      apple.setNewLocation()
+    else:
+      del self.wormCoords[-1]  # Ngược lại, phần thân cuối cùng của con rắn được loại bỏ để tạo hiệu ứng di chuyển.
+      
+    # vị trí mới của đầu con rắn được tạo thành từ tọa độ x và y của đầu con rắn hiện tại và được thêm vào phần đầu của mảng wormCoords.
+    if self.direction == self.UP:
+      newHead = {'x': self.wormCoords[self.HEAD]['x'],
+                 'y': self.wormCoords[self.HEAD]['y'] - 1}
+    elif self.direction == self.DOWN:
+      newHead = {'x': self.wormCoords[self.HEAD]['x'],
+                 'y': self.wormCoords[self.HEAD]['y'] + 1}
+    elif self.direction == self.LEFT:
+      newHead = {'x': self.wormCoords[self.HEAD]['x'] - 1,
+                 'y': self.wormCoords[self.HEAD]['y']}
+    elif self.direction == self.RIGHT:
+      newHead = {'x': self.wormCoords[self.HEAD]['x'] + 1,
+                 'y': self.wormCoords[self.HEAD]['y']}
+    self.wormCoords.insert(0, newHead)
